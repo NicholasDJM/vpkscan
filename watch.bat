b64042e53e5146c14184ab1512bd01f145572e54
NicholasDJM <11604027+NicholasDJM@users.noreply.github.com>
2022-08-02 04:23:40 -0400

    Updated HTML output location

--- watch.bat
+++ watch.bat
@@ -1,3 +1,3 @@
 @echo off
-start "Pug CLI" cmd /c pug -P -w ./src/main.pug ./src/licenses.pug -o ./resources^&pause
+start "Pug CLI" cmd /c pug -P -w ./src/main.pug ./src/licenses.pug -o ./resources/html^&pause
 start "WindiCSS CLI" cmd /c windicss ./resources/main.html ./resources/licenses.html -a --dev --output ./resources/windi.css^&pause
