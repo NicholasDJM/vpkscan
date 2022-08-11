e55dead06b04e733502ab6e5749c393bc1e890f3
NicholasDJM <11604027+NicholasDJM@users.noreply.github.com>
2022-08-11 16:17:18 -0400

    Added check to build.bat if inspector is enabled.

--- build.bat
+++ build.bat
@@ -1,3 +1,11 @@
+@echo off
+set filetime=%time%
+set filetime=%filetime::=%
+findstr "enableInspector" "neutralino.config.json" > %tmp%\%filetime%.txt
+set /p string=<%tmp%\%filetime%.txt
+set string=%string:~-5,4%
+if "%string%" == "true" choice /N /M "Inspector is still enabled! Continue? (Y/N)"
+if %errorlevel% EQU 2 exit /b
 rmdir /q /s dist
 start "title" /b /wait cmd /c "neu build"
 copy ".\dist\vpkscan\resources.neu" ".\dist"
