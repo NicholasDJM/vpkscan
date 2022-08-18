ed8cf24ab6ef9d2f9f21964ada0d4b28b43c8a60
NicholasDJM <11604027+NicholasDJM@users.noreply.github.com>
2022-08-18 01:35:45 -0400

    Various minor improvements.

--- build.bat
+++ build.bat
@@ -1,4 +1,6 @@
 @echo off
+set timeout = 2
+REM Timeout must be 2 or more. If the css file is not given enough time to generate, increase the above value.
 set filetime=%time%
 set filetime=%filetime::=%
 set config=neutralino.config.json
@@ -16,6 +18,13 @@ if [%one%]==[/r] (
 )
 exit /b
 :release
+	start "Pug CLI" /b /wait cmd /c pug ./src/main.pug ./src/licenses.pug -b resources -o ./resources/html
+	del /Q ".\resources\windi.css"
+	REM We're using conncurrently to kill windicss. We require dev mode to correctly generate css classes, but it doesn't exit automatically.
+	start "WindiCSS CLI" /b /wait cmd /c "npx concurrently --kill-others "windicss ./resources/html/main.html ./resources/html/licenses.html -a -m --dev --output ./resources/windi.css" "ping localhost -n 2""
+	start "ESLint" /b /wait cmd /c "pnpm run eslint"
+	start "Stylelint First Pass" /b /wait cmd /c "pnpm run stylelint"
+	start "Stylelint Second Pass" /b /wait cmd /c "pnpm run stylelint"
 	start "title" /b /wait cmd /c "neu build -r"
 	findstr "version" "%config%" > "%tmp%\%filetime%.txt"
 	set /p string=<"%tmp%\%filetime%.txt"
--- resources/js/licenses.js
+++ resources/js/licenses.js
@@ -1,7 +1,7 @@
 const getLocalString = window.getLocalString;
 $(()=>{
 	$("[data-href]").each((index, element)=>{
-		let url = $(element).attr("data-href");
+		const url = $(element).attr("data-href");
 		$(element).on("click", (event)=>{
 			event.preventDefault();
 			Neutralino.os.open(url);
